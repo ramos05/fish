4470a832860982955afdfdf41f343a3dcbf308ff
Ram <ramos16qa@gmail.com>
2020-09-11 00:50:30 -0400

    1

--- src/FishTank.java
+++ src/FishTank.java
@@ -5,22 +5,28 @@ public class FishTank {
 
     public static void main(String[] args) {
 
-        ArrayList fishTank = new ArrayList();
+        ArrayList<Fish> fishTank = new ArrayList();
+
+
         Fish theFish;
-        Fish aFish = new Fish(20);
+        Fish aFish = new Fish(10, 2, "Red");
         aFish.color = "красную";
         aFish.weight = 2;
         fishTank.add(aFish);
-        aFish = new Fish(10);
-        aFish.color = "зеленую";
+        aFish = new Fish(10, 2, "Red");
+        aFish.color = "purple";
+        aFish.weight = 12;
+        fishTank.add(aFish);
+        aFish = new Fish("Blue");
+//        aFish.color = "зеленую";
         aFish.weight = 5;
         fishTank.add(aFish);
 
 
         int fishCount = fishTank.size();
-        for (int i=0;i<fishCount; i++){
-            theFish = (Fish) fishTank.get(i);
-            System.out.println("Поймал "+ theFish.color + " рыбу с весом " + theFish.weight + " кг. Глубина:" + theFish.currentDepth);
+        for (int i = 0; i < fishCount; i++) {
+            theFish = fishTank.get(i);
+            System.out.println("Поймал " + theFish.color + " рыбу с весом " + theFish.weight + " кг. Глубина:" + theFish.currentDepth);
         }
     }
 }
