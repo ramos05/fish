447d7fe4954dfc595ce5dbcdd4fccfdb6c9518d5
Ram <ramos16qa@gmail.com>
2020-09-11 01:54:41 -0400

    1

--- src/FishTank.java
+++ src/FishTank.java
@@ -13,20 +13,27 @@ public class FishTank {
         aFish.color = "красную";
         aFish.weight = 2;
         fishTank.add(aFish);
-        aFish = new Fish(10, 2, "Red");
+        aFish = new Fish(100, 42, "Wite");
         aFish.color = "purple";
         aFish.weight = 12;
         fishTank.add(aFish);
+//        fishTank.remove(aFish);
+//        fishTank.set(0,aFish);
         aFish = new Fish("Blue");
-//        aFish.color = "зеленую";
+        aFish.color = "зеленую";
         aFish.weight = 5;
         fishTank.add(aFish);
-
 
         int fishCount = fishTank.size();
         for (int i = 0; i < fishCount; i++) {
             theFish = fishTank.get(i);
             System.out.println("Поймал " + theFish.color + " рыбу с весом " + theFish.weight + " кг. Глубина:" + theFish.currentDepth);
+
+
+//            System.out.println(fishTank.get(2));
+
+
         }
+
     }
 }
